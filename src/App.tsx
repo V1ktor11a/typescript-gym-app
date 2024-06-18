0a96d71db9be4697ea88d43e9e11e60a6eebc8ec
Vika Sabadosh <vika.kahanec@gmail.com>
2024-06-18 16:18:31 +0300

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/scenes/navbar";
 import Home from "./scenes/home";
+import Benefits from "./scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -29,6 +33,10 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
